797ceaeac3e38d5aa80cdd58ac0c63fba1015bd7
Tejinder <tejinderwebdev@gmail.com>
2022-07-01 21:52:45 +0530

    shopping page added

---
+++ src/components/category-items/category-items.component.jsx
@@ -0,0 +1,27 @@
+const CategoryItem = (props) => {
+    const {title, imageUrl} = props.category;
+    return(
+        <div className="card bg-base-100 lg:w-1/4 md:w-1/2 p-4 w-full">
+               <div className="shadow-xl">
+                  <figure>
+                      <img
+                        src={imageUrl}
+                        alt={`Shoes ${title}`}
+                        className="w-[400px] h-[225px] object-cover"
+                      />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{title}</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Shop Now</button>
+                      </div>
+                    </div>
+               </div>
+                
+          </div>
+    )
+
+}
+
+export default CategoryItem;
---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,37 @@
+ import CategoryContainer from "../category-container/category-container.component";
+
+const Category = () => {
+    const categories = [
+        {
+          "id": 1,
+          "title": "Hats",
+          "imageUrl": "https://images.pexels.com/photos/7880314/pexels-photo-7880314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+          "id": 2,
+          "title": "Jackets",
+          "imageUrl": "https://images.pexels.com/photos/1833082/pexels-photo-1833082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+          "id": 3,
+          "title": "Sneakers",
+          "imageUrl": "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+          "id": 4,
+          "title": "Womens",
+          "imageUrl": "https://images.pexels.com/photos/1661471/pexels-photo-1661471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+          "id": 5,
+          "title": "Mens",
+          "imageUrl": "https://images.pexels.com/photos/775091/pexels-photo-775091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+      ];
+    return(
+        <div className="container px-5 py-24 mx-auto font-Roboto">
+                  <CategoryContainer categories={categories}/>
+        </div>
+    )
+}
+export default Category
