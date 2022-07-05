b63517e3be5b26d49effb177cb70793c389cf0c6
Tejinder <tejinderwebdev@gmail.com>
2022-07-05 09:50:47 +0530

    added sample Nav using Outlet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Home from './routes/home.component';
+import Home from "./routes/home.component";
 
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Outlet } from "react-router-dom";
 
+const Navigation = () => {
+  return (
+    <div>
+      <h2>I am Navigation component.</h2>
+      <Outlet />
+    </div>
+  );
+};
 
+const Shop = () => {
+  return <h1>I am a shop component</h1>;
+};
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,37 +1,44 @@
+import { Outlet } from "react-router-dom";
 import Directory from "../components/Directory/directory.component";
 function Home() {
-    const categories = [
-      {
-        "id": 1,
-        "title": "Hats",
-        "imageUrl": "https://images.pexels.com/photos/7880314/pexels-photo-7880314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 2,
-        "title": "Jackets",
-        "imageUrl": "https://images.pexels.com/photos/1833082/pexels-photo-1833082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 3,
-        "title": "Sneakers",
-        "imageUrl": "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 4,
-        "title": "Womens",
-        "imageUrl": "https://images.pexels.com/photos/1661471/pexels-photo-1661471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      {
-        "id": 5,
-        "title": "Mens",
-        "imageUrl": "https://images.pexels.com/photos/775091/pexels-photo-775091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      }
-    ];
-    return (
-      <div>
-         <Directory categories={categories} />
-      </div>
-    );
-  }
-  
-  export default Home;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl:
+        "https://images.pexels.com/photos/7880314/pexels-photo-7880314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl:
+        "https://images.pexels.com/photos/1833082/pexels-photo-1833082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl:
+        "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl:
+        "https://images.pexels.com/photos/1661471/pexels-photo-1661471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl:
+        "https://images.pexels.com/photos/775091/pexels-photo-775091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+  return (
+    <div>
+      <Outlet />
+      <Directory categories={categories} />
+    </div>
+  );
+}
+
+export default Home;
